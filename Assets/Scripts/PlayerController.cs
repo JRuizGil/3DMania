792e684ea3454c72fbd45f20330a5d89485eed1e
JRuizG <joseramon_ruiz@euneiz.com>
2025-03-03 19:32:22 +0100

    AssetsAdd, Player+CameraMovement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 2f;
+    public Transform cameraTransform;
+
+    private float rotationX = 0f;
+
+    void Update()
+    {
+        HandleMovement();
+        HandleMouseLook();
+        HandleExit();
+    }
+    void HandleMovement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.right * moveX + transform.forward * moveZ;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+    void HandleExit()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+        }
+    }
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
